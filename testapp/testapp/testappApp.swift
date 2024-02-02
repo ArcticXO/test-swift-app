8bc3d4f5fe9a97c7721b13eeb1851384ac586339
Ali Al Abdullah <38346718+arcticxo@users.noreply.github.com>
2024-02-02 13:26:28 +0000

    this is ain intial commit

---
+++ testapp/testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Ali Al Abdullah on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
